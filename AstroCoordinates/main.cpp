7ce829a782150f09a88e374cde224787b49e92ea
Josh <jvcard@iastate.edu>
2016-03-25 14:42:34 -0500

    Initial Commit

---
+++ AstroCoordinates/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AstroCoordinates
+//
+//  Created by Josh Cardenzana on 3/25/16.
+//  Copyright © 2016 JCardenzana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
