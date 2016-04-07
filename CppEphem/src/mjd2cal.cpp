b87c8edafa66f694d0337ad7c4e18074b7cf6b0b
Josh <jvcard@iastate.edu>
2016-04-07 13:47:46 -0500

    Minor updates to usage text for "jd2cal" and "mjd2cal" functions.

--- CppEphem/src/mjd2cal.cpp
+++ CppEphem/src/mjd2cal.cpp
@@ -11,15 +11,16 @@
 
 int main(int argc, const char * argv[]) {
     // Set the default return type ID
-    int return_format(1) ;
+    int return_format(0) ;
     
     // Check that we've been passed an actual argument
     if (argc < 2) {
         // Print some usage information
         std::cout << "\nUSAGE: mjd2jd <modified julian date> <return format ID>\n" ;
         std::cout << "RETURNED: Gregorian calendar date in two formats:\n" ;
-        std::cout << "      ID=1: YYYYMMDD.<date fraction>\n" ;
-        std::cout << "      ID=2: Year Month Day.<day fraction>\n\n" ;
+        std::cout << "   (default) ID=0: YYYYMMDD.<date fraction>\n" ;
+        std::cout << "             ID=1: YYYY MM DD.<day fraction>" ;
+        std::cout << "\n\n" ;
         return 0 ;
     } else if (argc == 3) {
         // Get the return format from the command line
@@ -29,7 +30,7 @@ int main(int argc, const char * argv[]) {
     // Convert the input variable to a double
     double mjd = std::stod(std::string(argv[1])) ;
     
-    if (return_format == 2) {
+    if (return_format == 1) {
         std::vector<double> gregorian_vect = CEDate::MJD2GregorianVect(mjd) ;
         std::printf("%4.0f %02.0f %06.4f\n",
                     gregorian_vect[0], gregorian_vect[1], gregorian_vect[2]+gregorian_vect[3]) ;
