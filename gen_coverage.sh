46b5e4e3ff98a539af3bc3a161f9c934f91c4884
Josh Cardenzana <jcardenzana@irap.omp.eu>
2018-02-01 16:28:55 +0100

    Updated coverage tests

--- gen_coverage.sh
+++ gen_coverage.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env
 
 if [ "x$CC" == "xgcc" ] ; then
-    gcov -o test/CMakeFiles/test_cppephem.dir/src/ ./test_cppephem
+    gcov -o test/CMakeFiles/test_cppephem.dir/src/ ./test_cppephem.cpp
 elif [ "x$CC" == "xclang" ] ; then
-    llvm-cov gcov -o test/CMakeFiles/test_cppephem.dir/src/ ./test_cppephem
+    llvm-cov gcov -o test/CMakeFiles/test_cppephem.dir/src/ ./test_cppephem.cpp
 else
     echo "\n\nNO APPROPRIATE CODE COVERAGE TOOL FOUND\n\n"
 fi
