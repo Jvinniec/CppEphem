c8a504087c591cc193d82bb85f639f5781facc3d
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-02-04 00:36:41 +0100

    Add CEBody tests

--- cppephem/test/test_CEBody.cpp
+++ cppephem/test/test_CEBody.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *  test_CECoordinates.cpp: CppEphem                                       *
+ *  test_CEBody.cpp: CppEphem                                              *
  * ----------------------------------------------------------------------- *
  *  Copyright © 2018 JCardenzana                                           *
  * ----------------------------------------------------------------------- *
@@ -19,27 +19,27 @@
  *                                                                         *
  ***************************************************************************/
 
-#include "test_CECoordinates.h"
+#include "test_CEBody.h"
 #include "CENamespace.h"
 
 
 /**********************************************************************//**
  * Default constructor
  *************************************************************************/
-test_CECoordinates::test_CECoordinates() :
+test_CEBody::test_CEBody() :
     CETestSuite()
 {
-    // Lets use the Crab Nebula (M1) for our testing
-    base_ = CECoordinates(83.633, 22.0145, 
-                          CECoordinateType::ICRS, 
-                          CEAngleType::DEGREES);
+    // Let's use the Crab Nebula
+    base_ = CEBody("Crab", 83.633, 22.0145, 
+                   CECoordinateType::ICRS, 
+                   CEAngleType::DEGREES);
 }
 
 
 /**********************************************************************//**
  * Destructor
  *************************************************************************/
-test_CECoordinates::~test_CECoordinates()
+test_CEBody::~test_CEBody()
 {}
 
 
@@ -48,72 +48,52 @@ test_CECoordinates::~test_CECoordinates()
  * 
  * @return whether or not all tests succeeded
  *************************************************************************/
-bool test_CECoordinates::runtests()
+bool test_CEBody::runtests()
 {
-    std::cout << "Testing CECoordinates:\n";
+    std::cout << "Testing CEBody:\n";
 
     // Run each of the tests
-    update_pass(test_SetCoord_Icrs());
-    update_pass(test_SetCoord_Cirs());
-    update_pass(test_SetCoord_Galactic());
+    update_pass(test_Name());
+    update_pass(test_GetCoordinates());
 
     return pass();
 }
 
 
 /**********************************************************************//**
- * Test ability to set coordinates as CIRS
+ * Run tests
+ * 
+ * @return whether or not all tests succeeded
  *************************************************************************/
-bool test_CECoordinates::test_SetCoord_Cirs()
+bool test_CEBody::test_Name(void)
 {
-    // Get the CIRS coordinates from the base_coord_ object
-    CECoordinates base_cirs = base_.ConvertToCIRS(CppEphem::julian_date_J2000());
+    // Check that we can get the name
+    update_pass(test_string(base_.Name(), "Crab"));
 
-    // Create a new coordinates from the values passed by test_coord
-    CECoordinates test_coord;
-    test_coord.SetCoordinates(base_cirs.XCoordinate_Deg(), 
-                              base_cirs.YCoordinate_Deg(),
-                              CECoordinateType::CIRS, 
-                              CEAngleType::DEGREES);
-    
-    // Compare the coordinates
-    return test_bool((test_coord == base_cirs), true);
+    // Check that we can set the name
+    CEBody test_body(base_);
+    std::string new_name = "NotCrab";
+    test_body.SetName(new_name);
+    update_pass(test_string(test_body.Name(), new_name));
+
+    return pass();
 }
 
 
 /**********************************************************************//**
- * Test ability to set coordinates as ICRS
+ * Run tests
+ * 
+ * @return whether or not all tests succeeded
  *************************************************************************/
-bool test_CECoordinates::test_SetCoord_Icrs()
+bool test_CEBody::test_GetCoordinates(void)
 {
-    // Create a new coordinates from the values passed by test_coord
-    CECoordinates test_coord;
-    test_coord.SetCoordinates(base_.XCoordinate_Deg(), 
-                              base_.YCoordinate_Deg(),
-                              CECoordinateType::ICRS, 
-                              CEAngleType::DEGREES);
-
-    // Compare the coordinates
-    return test_bool((test_coord == base_), true);
-}
-
-/**********************************************************************//**
- * Test ability to set coordinates as Galactic
- *************************************************************************/
-bool test_CECoordinates::test_SetCoord_Galactic()
-{
-    // Get the CIRS coordinates from the base_coord_ object
-    CECoordinates base_galactic = base_.ConvertToGalactic(CppEphem::julian_date_J2000());
-
-    // Create a new coordinates from the values passed by test_coord
-    CECoordinates test_coord;
-    test_coord.SetCoordinates(base_galactic.XCoordinate_Deg(), 
-                              base_galactic.YCoordinate_Deg(),
-                              CECoordinateType::CIRS, 
-                              CEAngleType::DEGREES);
+    // Test getting the coordinates
+    CECoordinates test_coords = base_.GetCoordinates();
     
-    // Compare the coordinates
-    return test_bool((test_coord == base_galactic), true);
+    // Test that the coordinates are equal
+    update_pass(test_bool(test_coords == base_, true));
+
+    return pass();
 }
 
 
@@ -121,6 +101,6 @@ bool test_CECoordinates::test_SetCoord_Galactic()
  * Main method that actually runs the tests
  *************************************************************************/
 int main(int argc, char** argv) {
-    test_CECoordinates tester;
+    test_CEBody tester;
     return tester.runtests();
 }
