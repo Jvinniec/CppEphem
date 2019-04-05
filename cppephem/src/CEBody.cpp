0b6ca2d5c21186cded3b1872a52179617c3cdf27
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-04-05 18:25:43 +0200

    Update CEBody with explicit assignment operator

--- cppephem/src/CEBody.cpp
+++ cppephem/src/CEBody.cpp
@@ -32,9 +32,10 @@
 /**********************************************************************//**
  * Default constructor
  *************************************************************************/
-CEBody::CEBody() :
-    name_("NONE")
-{}
+CEBody::CEBody() : CECoordinates()
+{
+    init_members();
+}
 
 /**********************************************************************//**
  * Primary constructor
@@ -49,31 +50,100 @@ CEBody::CEBody(const std::string& name,
                const double& xcoord, const double& ycoord,
                const CECoordinateType& coord_type,
                const CEAngleType& angle_type) :
-    CECoordinates(xcoord, ycoord, coord_type, angle_type),
-    name_(name)
-{}
+    CECoordinates(xcoord, ycoord, coord_type, angle_type)
+{
+    init_members();
+    SetName(name);
+}
 
 /**********************************************************************//**
- * Copy constructor from a single set of coordinates
- * @param[in] coords       coordinates object
+ * Copy constructor
+ * @param[in] other         CEBody object to copy from
+ * @param[in] name          New object name
  *************************************************************************/
-CEBody::CEBody(const CEBody &coords, const std::string& name) :
-    CECoordinates(coords),
-    name_(name)
-{}
+CEBody::CEBody(const CEBody &other, const std::string& name) :
+    CECoordinates(other)
+{
+    init_members();
+    copy_members(other);
+    SetName(name);
+}
 
 /**********************************************************************//**
  * Copy constructor from a single set of coordinates
  * @param[in] coords       coordinates object
  *************************************************************************/
 CEBody::CEBody(const CECoordinates &coords, const std::string& name) :
-    CECoordinates(coords),
-    name_(name)
-{}
+    CECoordinates(coords)
+{
+    init_members();
+    SetName(name);
+}
 
 /**********************************************************************//**
  * Destructor
  *************************************************************************/
 CEBody::~CEBody()
-{}
+{
+    free_members();
+}
 
+
+/*--------------------------------------------------*
+ *                   Overloaded operators
+ *--------------------------------------------------*/
+
+
+/**********************************************************************//**
+ * Overloaded assignment operator
+ * @param[in] other     CEBody object to be copied
+ * @return Reference to this object post-copy
+ *************************************************************************/
+CEBody& CEBody::operator=(const CEBody& other)
+{
+    if (this != &other) {
+        // Copy parent class parameters
+        this->CECoordinates::operator=(other);
+
+        // Reset this object and copy values from 'other'
+        free_members();
+        init_members();
+        copy_members(other);
+    }
+    return *this;
+}
+
+/*--------------------------------------------------*
+ *                   Public methods
+ *--------------------------------------------------*/
+
+
+/*--------------------------------------------------*
+ *                  Private methods
+ *--------------------------------------------------*/
+
+
+/**********************************************************************//**
+ * Free all allocated data members
+ *************************************************************************/
+void CEBody::free_members(void)
+{
+}
+
+
+/**********************************************************************//**
+ * Initialize data members
+ *************************************************************************/
+void CEBody::init_members(void)
+{
+    name_ = "undefined";
+}
+
+
+/**********************************************************************//**
+ * Copy all data members
+ *************************************************************************/
+void CEBody::copy_members(const CEBody& other)
+{
+    name_ = other.name_;
+}
