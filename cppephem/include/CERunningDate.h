d9e3b06132ccf46d91a10b527cd8c28e9180f289
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-04-11 11:22:51 +0200

    Update CERunning date with copy assignment operator Add dedicated initizizer method Add dedicated copy method Add dedicated deallocation method

--- cppephem/include/CERunningDate.h
+++ cppephem/include/CERunningDate.h
@@ -30,8 +30,11 @@
 class CERunningDate : public CEDate {
 public:
     CERunningDate() ;
+    CERunningDate(const CERunningDate& other);
     virtual ~CERunningDate() ;
     
+    CERunningDate& operator=(const CERunningDate& other);
+
     // Some overloaded methods to make sure that the current
     // values are obtained first
     virtual double JD() const;
@@ -39,34 +42,74 @@ public:
     virtual double Gregorian() const;
     
     // Method that can be used to change the date that is stored in this object
-    virtual void SetDate(double date=CEDate::CurrentJD(), CEDateType time_format=CEDateType::JD) ;
+    virtual void SetDate(const double&     date=CEDate::CurrentJD(), 
+                         const CEDateType& time_format=CEDateType::JD) ;
     // Method for setting the dates from the Gregorian calendar dates
     virtual void SetDate(std::vector<double> date) ;
     
     /// Method for getting the number of seconds since this object was created or reset
-    virtual double RunTime() const;
+    virtual double RunTime(void) const;
+    double         ScaledRunTime(void) const;
+    void           ResetTime(void);
+    virtual double GetTimerSpeed(void) const;
+    virtual void   SetTimerSpeed(const double& scale=1.0);
+    
+private:
 
-    /// Returns the ellapsed time scaled by some factor
-    inline double ScaledRunTime() const
-        {return timer_speed_factor_ * RunTime() ;}
-    /// Method for resetting the 'start' variable
-    inline void ResetTime()
-        {start = std::chrono::high_resolution_clock::now();}
-    
-    /// Get the rate at which the timer is progressing
-    ///     @return Time scale factor
-    virtual double GetTimerSpeed() {return timer_speed_factor_ ;}
-    /// Set the rate at which the timer progresses
-    ///     @param scale    Time scale factor
-    virtual void SetTimerSpeed(double scale=1.0) {timer_speed_factor_ = scale;}
-    
-protected:
+    void copy_members(const CERunningDate& other);
+    void init_members(void);
+    void free_members(void);
+
     /// Variable to hold the starting time. Additional updates will be computed from
     /// this start time.
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now() ;
+    std::chrono::high_resolution_clock::time_point start_;
     
     /// Variable that can be used to speed up, slow down, or reverse the rate of time
-    double timer_speed_factor_ = 1.0 ;
+    double timer_speed_factor_;
 };
+
+
+/**********************************************************************//**
+ * Get the rate at which the timer is progressing
+ * 
+ * @return Time scale factor
+ *************************************************************************/
+inline
+double CERunningDate::GetTimerSpeed(void) const
+{
+    return timer_speed_factor_ ;
+}
+
+
+/**********************************************************************//**
+ * Method for resetting the 'start' variable
+ *************************************************************************/ 
+inline
+void CERunningDate::ResetTime(void)
+{
+    start_ = std::chrono::high_resolution_clock::now();
+}
+
+/**********************************************************************//**
+ * Set the rate at which the timer progresses
+ * 
+ * @param scale    Time scale factorvirtual 
+ *************************************************************************/
+inline
+void CERunningDate::SetTimerSpeed(const double& scale) 
+{
+    timer_speed_factor_ = scale;
+}
+
+/**********************************************************************//**
+ * Return the scaled ellapsed time since this object was created
+ * 
+ * @return Ellapsed time since object was created (seconds)
+ *************************************************************************/
+inline 
+double CERunningDate::ScaledRunTime(void) const
+{
+    return timer_speed_factor_ * RunTime();
+}
 
 #endif /* CERunningDate_h */
