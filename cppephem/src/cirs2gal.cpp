d230a368efc79c5cf610ea7ca2f194ceadcf4a4b
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-05-03 14:01:19 +0200

    Update cirs2gal and associated build tests

--- cppephem/src/cirs2gal.cpp
+++ cppephem/src/cirs2gal.cpp
@@ -27,114 +27,27 @@
 
 // CppEphem HEADERS
 #include "CppEphem.h"
+#include "CLOptions.h"
 
 /**********************************************************************//**
+ * Define the command line options for this program
  *************************************************************************/
-void Print_Help()
+CLOptions DefineOpts()
 {
-    // This is the help text to be printed if no command line options are provide
-    // or if the '--help' or '-h' options are given
-    std::printf("\nUSAGE: cirs2gal [options]\n") ;
+    CLOptions opts ;
+    // Set the options
+    opts.AddDoubleParam("r,ra", "CIRS right ascension for the conversion (degrees)",0.0) ;
+    opts.AddDoubleParam("d,dec", "CIRS declination for the conversion (degrees)", 0.0) ;
+    opts.AddDoubleParam("j,juliandate", "Julian date for query (default is current JD)", 
+                        CEDate::CurrentJD());
     
-    std::printf("\nREQURED OPTIONS:\n") ;
-    std::printf("  --ra,          -R      Right Ascension (degrees)\n") ;
-    std::printf("  --dec,         -D      Declination (degrees)\n") ;
-    
-    std::printf("\nADDITIONAL OPTIONS:\n") ;
-    std::printf("  --help,        -h      Prints help text\n") ;
-    std::printf("  --juliandate,  -j      Julian Date for query (default=%f)\n", CppEphem::julian_date_J2000()) ;
-    
-    std::printf("\n") ;
+    return opts;
 }
 
-/**********************************************************************//**
- *************************************************************************/
-std::map<std::string, double> defaultoptions()
-{
-    // Define the default values of some of the unnecessary parameters
-    std::map<std::string, double> options ;
-    
-    // Set January 1, 2000 as the default date
-    options["juliandate"] = CppEphem::julian_date_J2000() ;
-    
-    return options ;
-}
 
 /**********************************************************************//**
  *************************************************************************/
-void getoptions(struct option* longopts)
-{
-    longopts[0] = {"help",        no_argument,       0, 'h'} ;   // Print help information
-    longopts[1] = {"ra",          required_argument, 0, 'R'} ;   // CIRS right ascension
-    longopts[2] = {"dec",         required_argument, 0, 'D'} ;   // CIRS declination
-    longopts[3] = {"juliandate",  required_argument, 0, 'j'} ;   // Julian date of observation
-    longopts[4] = {0,0,0,0} ;
-}
-
-/**********************************************************************//**
- *************************************************************************/
-std::map<std::string, double> parseoptions(int argc, char** argv, const struct option* longopts)
-{
-    // Setup the default parameters
-    std::map<std::string, double> options = defaultoptions() ;
-    
-    int c;
-    
-    while (1)
-    {
-        /* getopt_long stores the option index here. */
-        int option_index = 0;
-        
-        c = getopt_long (argc, argv, "hR:D:j:",
-                         longopts, &option_index);
-        
-        /* Detect the end of the options. */
-        if (c == -1) break;
-        
-        switch (c)
-        {
-            case 0:
-                /* If this option set a flag, do nothing else now. */
-                if (longopts[option_index].flag != 0)
-                    break;
-                
-                // Set the option
-                options[longopts[option_index].name] = std::stod(optarg) ;
-                break;
-                
-            case 'h':
-                // Print the help message and quit
-                Print_Help() ;
-                return std::map<std::string, double>() ;
-                
-            case 'R':
-                options["ra"] = std::stod(optarg) ;
-                break;
-                
-            case 'D':
-                options["dec"] = std::stod(optarg) ;
-                break;
-                
-            case 'j':
-                options["juliandate"] = std::stod(optarg) ;
-                break;
-
-            case '?':
-                /* getopt_long already printed an error message. */
-                break;
-                
-            default:
-                break ;
-        }
-    }
-    
-    
-    return options ;
-}
-
-/**********************************************************************//**
- *************************************************************************/
-void PrintResults(std::map<std::string, double> inputs, std::map<std::string, double> results)
+void PrintResults(CLOptions& opts, std::map<std::string, double> results)
 {
     std::printf("\n") ;
     std::printf("******************************************\n") ;
@@ -144,31 +57,19 @@ void PrintResults(std::map<std::string, double> inputs, std::map<std::string, do
     std::printf("    Galactic Lon.: %f degrees\n", results["longitude"]*DR2D) ;
     std::printf("    Galactic Lat.: %+f degrees\n", results["latitude"]*DR2D) ;
     std::printf("CIRS Coordinates (input)\n") ;
-    std::printf("    Right Ascension: %f degrees\n", inputs["ra"]) ;
-    std::printf("    Declination    : %+f degrees\n", inputs["dec"]) ;
-    std::printf("    Julian Date    : %f\n", inputs["juliandate"]) ;
+    std::printf("    Right Ascension: %f degrees\n", opts.AsDouble("ra")) ;
+    std::printf("    Declination    : %+f degrees\n", opts.AsDouble("dec")) ;
+    std::printf("    Julian Date    : %f\n", opts.AsDouble("juliandate")) ;
     std::printf("\n") ;
 }
 
 /**********************************************************************//**
  *************************************************************************/
-int main(int argc, char** argv) {
-    
-    // If no arguments have been passed, print the help text and quit
-    if (argc==1) {
-        Print_Help() ;
-        return 0 ;
-    }
-    
-    // Define the optional arguments
-    struct option longopts[5] ;
-    getoptions(longopts) ;
-    
-    // Parse the options
-    std::map<std::string, double> options = parseoptions(argc, argv, longopts) ;
-    
-    // If no valid options were given, quit
-    if (options.empty()) return 0 ;
+int main(int argc, char** argv) 
+{    
+    // Get the options from the command line
+    CLOptions opts = DefineOpts() ;
+    if (opts.ParseCommandLine(argc, argv)) return 0 ;
     
     // Create a map to store the results
     std::map<std::string, double> results ;
@@ -176,14 +77,15 @@ int main(int argc, char** argv) {
     results["latitude"]  = 0.0 ;
     
     // Convert the coordinates
-    CECoordinates::CIRS2Galactic(options["ra"]*DD2R,
-                                 options["dec"]*DD2R,
+    CEDate date(opts.AsDouble("juliandate"), CEDateType::JD);
+    CECoordinates::CIRS2Galactic(opts.AsDouble("ra")*DD2R,
+                                 opts.AsDouble("dec")*DD2R,
                                  &results["longitude"],
                                  &results["latitude"],
-                                 CEDate(options["juliandate"])) ;
+                                 date);
     
     // Print the results
-    PrintResults(options, results) ;
+    PrintResults(opts, results) ;
     
     return 0 ;
 }
