c0a92a3451d8faec07dbd1f530fce136ba391281
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-05-03 15:03:08 +0200

    update obs2gal short param names to be consistent with other executables

--- cppephem/src/obs2gal.cpp
+++ cppephem/src/obs2gal.cpp
@@ -39,16 +39,16 @@ CLOptions DefineOpts()
     
     // Setup the defaults
     CEObserver obs ;
-    opts.AddDoubleParam("X,longitude","Observer longitude (degrees, East-positive)",obs.Longitude_Deg()) ;
-    opts.AddDoubleParam("Y,latitude", "Observer latitude (degrees)", obs.Latitude_Deg()) ;
-    opts.AddDoubleParam("A,azimuth", "Azimuth (degrees, positive North=0, East=90)", 0.0) ;
-    opts.AddDoubleParam("Z,zenith", "Zenith angle (degrees)", 0.0) ;
-    opts.AddDoubleParam("j,juliandate", "Julian date for query (default is current time)", CEDate::CurrentJD()) ;
-    opts.AddDoubleParam("e,elevation", "Observer elevation (meters above sea level)", obs.Elevation_m()) ;
-    opts.AddDoubleParam("r,humidity", "Observer's relative humidity (0-1)", obs.RelativeHumidity()) ;
-    opts.AddDoubleParam("p,pressure","Observer's atmospheric pressure (hPa)",obs.Pressure_hPa()) ;
-    opts.AddDoubleParam("t,temperature","Observer's atmospheric temperature (degrees Celsius)", obs.Temperature_C()) ;
-    opts.AddDoubleParam("w,wavelength","Wavelength of light being observed (micrometers)",obs.Wavelength_um()) ;
+    opts.AddDoubleParam("x,longitude","Observer longitude (degrees, East-positive)",obs.Longitude_Deg());
+    opts.AddDoubleParam("y,latitude", "Observer latitude (degrees)", obs.Latitude_Deg());
+    opts.AddDoubleParam("a,azimuth", "Azimuth (degrees, positive North=0, East=90)", 0.0);
+    opts.AddDoubleParam("z,zenith", "Zenith angle (degrees)", 0.0);
+    opts.AddDoubleParam("j,juliandate", "Julian date for query (default is current time)", CEDate::CurrentJD());
+    opts.AddDoubleParam("e,elevation", "Observer elevation (meters above sea level)", obs.Elevation_m());
+    opts.AddDoubleParam("r,humidity", "Observer's relative humidity (0-1)", obs.RelativeHumidity());
+    opts.AddDoubleParam("p,pressure","Observer's atmospheric pressure (hPa)",obs.Pressure_hPa());
+    opts.AddDoubleParam("t,temperature","Observer's atmospheric temperature (degrees Celsius)", obs.Temperature_C());
+    opts.AddDoubleParam("w,wavelength","Wavelength of light being observed (micrometers)",obs.Wavelength_um());
     
     return opts ;
 }
