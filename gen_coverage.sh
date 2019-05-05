c7141185f05e532bd73cfda7f2029cf61b0bb167
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-05-05 21:51:00 +0200

    Add building and testing for cirs2icrs and gal2icrs

--- gen_coverage.sh
+++ gen_coverage.sh
@@ -31,26 +31,43 @@ if [[ "$TRAVIS_OS_NAME" == "linux" && "$CC" == "clang" ]] ; then
     LLVM_PROFILE_FILE="${outdir}/CETime.profraw"        ./build/bin/test_CETime
 
     # Generic executables
-    LLVM_PROFILE_FILE="${outdir}/angsep.profraw"        ./build/bin/test_angsep --xcoord1=0.0 --ycoord1=0.0 --xcoord2=0.0 --ycoord2=1.0
-    LLVM_PROFILE_FILE="${outdir}/planetephem.profraw"   ./build/bin/test_planetephem --planet=4 --longitude=0 --latitude=0 --elevation=0 --startJD=2451545.0
+    LLVM_PROFILE_FILE="${outdir}/angsep1.profraw"       ./build/bin/test_angsep --xcoord1=0.0 --ycoord1=0.0 --xcoord2=0.0 --ycoord2=1.0
+    LLVM_PROFILE_FILE="${outdir}/angsep2.profraw"       ./build/bin/test_angsep --xcoord1=0.0 --ycoord1=0.0 --xcoord2=0.0 --ycoord2=0.01745329251 --InputDegrees=0 --OutputDegrees=0
 
-    # Date conversion tests
-    LLVM_PROFILE_FILE="${outdir}/cal2jd.profraw"        ./build/bin/test_cal2jd 20000101.5
-    LLVM_PROFILE_FILE="${outdir}/cal2mjd.profraw"       ./build/bin/test_cal2mjd 20000101.5
-    LLVM_PROFILE_FILE="${outdir}/jd2cal.profraw"        ./build/bin/test_jd2cal 2458516.0
-    LLVM_PROFILE_FILE="${outdir}/jd2mjd.profraw"        ./build/bin/test_jd2mjd 2458516.0
-    LLVM_PROFILE_FILE="${outdir}/mjd2cal.profraw"       ./build/bin/test_mjd2cal 58515.5
-    LLVM_PROFILE_FILE="${outdir}/mjd2jd.profraw"        ./build/bin/test_mjd2jd 58515.5
+    LLVM_PROFILE_FILE="${outdir}/planetephem.profraw"   ./build/bin/test_planetephem --planet=4 --longitude=0 --latitude=42 --elevation=0 --startJD=2451545.0
+
+    # Date conversions (multiple versions to allow testing all the code)
+    LLVM_PROFILE_FILE="${outdir}/cal2jd1.profraw"       ./build/bin/test_cal2jd
+    LLVM_PROFILE_FILE="${outdir}/cal2jd2.profraw"       ./build/bin/test_cal2jd 20000101.5
+    LLVM_PROFILE_FILE="${outdir}/cal2jd3.profraw"       ./build/bin/test_cal2jd 2000 1 1.5
+
+    LLVM_PROFILE_FILE="${outdir}/cal2mjd1.profraw"      ./build/bin/test_cal2mjd
+    LLVM_PROFILE_FILE="${outdir}/cal2mjd2.profraw"      ./build/bin/test_cal2mjd 20000101.5
+    LLVM_PROFILE_FILE="${outdir}/cal2mjd3.profraw"      ./build/bin/test_cal2mjd 2000 1 1.5
+
+    LLVM_PROFILE_FILE="${outdir}/jd2cal1.profraw"       ./build/bin/test_jd2cal
+    LLVM_PROFILE_FILE="${outdir}/jd2cal2.profraw"       ./build/bin/test_jd2cal 2458516.0
+    LLVM_PROFILE_FILE="${outdir}/jd2cal3.profraw"       ./build/bin/test_jd2cal 2458516.0 1
+
+    LLVM_PROFILE_FILE="${outdir}/jd2mjd1.profraw"       ./build/bin/test_jd2mjd
+    LLVM_PROFILE_FILE="${outdir}/jd2mjd2.profraw"       ./build/bin/test_jd2mjd 2458516.0
+
+    LLVM_PROFILE_FILE="${outdir}/mjd2cal1.profraw"      ./build/bin/test_mjd2cal
+    LLVM_PROFILE_FILE="${outdir}/mjd2cal2.profraw"      ./build/bin/test_mjd2cal 58515.5
+    LLVM_PROFILE_FILE="${outdir}/mjd2cal3.profraw"      ./build/bin/test_mjd2cal 58515.5 1
+
+    LLVM_PROFILE_FILE="${outdir}/mjd2jd1.profraw"       ./build/bin/test_mjd2jd
+    LLVM_PROFILE_FILE="${outdir}/mjd2jd2.profraw"       ./build/bin/test_mjd2jd 58515.5
 
     # Coordinate conversion tests
     LLVM_PROFILE_FILE="${outdir}/cirs2gal.profraw"      ./build/bin/test_cirs2gal --ra=83.633 --dec=22.0145 --juliandate=2451545.0
-    #LLVM_PROFILE_FILE="${outdir}/cirs2icrs.profraw"     ./build/bin/test_cirs2icrs --ra=83.633 --dec=22.0145
+    LLVM_PROFILE_FILE="${outdir}/cirs2icrs.profraw"     ./build/bin/test_cirs2icrs --ra=83.633 --dec=22.0145 --juliandate=2451545.0
     LLVM_PROFILE_FILE="${outdir}/cirs2obs.profraw"      ./build/bin/test_cirs2obs --ra=83.633 --dec=22.0145 --longitude=0.0 --latitude=0.0 --juliandate=2451545.0
     LLVM_PROFILE_FILE="${outdir}/icrs2cirs.profraw"     ./build/bin/test_icrs2cirs --ra=83.633 --dec=22.0145 --juliandate=2451545.0
     LLVM_PROFILE_FILE="${outdir}/icrs2gal.profraw"      ./build/bin/test_icrs2gal --ra=83.633 --dec=22.0145
     LLVM_PROFILE_FILE="${outdir}/icrs2obs.profraw"      ./build/bin/test_icrs2obs --ra=83.633 --dec=22.0145 --juliandate=2451545.0
     LLVM_PROFILE_FILE="${outdir}/gal2cirs.profraw"      ./build/bin/test_gal2cirs --glon=184.553 --glat=-5.788 --juliandate=2451545.0
-    #LLVM_PROFILE_FILE="${outdir}/gal2icrs.profraw"      ./build/bin/test_gal2icrs --glon=184.553 --glat=-5.788
+    LLVM_PROFILE_FILE="${outdir}/gal2icrs.profraw"      ./build/bin/test_gal2icrs --glon=184.553 --glat=-5.788
     LLVM_PROFILE_FILE="${outdir}/gal2obs.profraw"       ./build/bin/test_gal2obs --glon=184.553 --glat=-5.788 --longitude=0.0 --latitude=0.0  --juliandate=2451545.0
     LLVM_PROFILE_FILE="${outdir}/obs2cirs.profraw"      ./build/bin/test_obs2cirs --longitude=0.0 --latitude=0.0 --azimuth=180 --zenith=20 --juliandate=2451545.0
     LLVM_PROFILE_FILE="${outdir}/obs2icrs.profraw"      ./build/bin/test_obs2icrs --longitude=0.0 --latitude=0.0 --azimuth=180 --zenith=20 --juliandate=2451545.0
@@ -86,11 +103,13 @@ if [[ "$TRAVIS_OS_NAME" == "linux" && "$CC" == "clang" ]] ; then
         -object ./build/bin/test_mjd2cal \
         -object ./build/bin/test_mjd2jd \
         -object ./build/bin/test_cirs2gal \
+        -object ./build/bin/test_cirs2icrs \
         -object ./build/bin/test_cirs2obs \
         -object ./build/bin/test_icrs2cirs \
         -object ./build/bin/test_icrs2gal \
         -object ./build/bin/test_icrs2obs \
         -object ./build/bin/test_gal2cirs \
+        -object ./build/bin/test_gal2icrs \
         -object ./build/bin/test_gal2obs \
         -object ./build/bin/test_obs2cirs \
         -object ./build/bin/test_obs2icrs \
